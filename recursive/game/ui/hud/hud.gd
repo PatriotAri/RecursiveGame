bfd48026aa864c5ab47cc9ecd4373befde328b5c
PatriotAri <ethan.ramsey98@gmail.com>
2026-05-10 19:05:05 -0500

    Fix soul counter.

--- recursive/game/ui/hud/hud.gd
+++ recursive/game/ui/hud/hud.gd
@@ -2,9 +2,11 @@ extends CanvasLayer
 
 @onready var health_bar: Control = $HealthBar
 @onready var gold_counter: Control = $GoldCounter
+@onready var soul_counter: Control = $SoulCounter
 
 func _ready() -> void:
 	var player := get_tree().get_first_node_in_group(&"player")
 	if player and "data" in player:
 		health_bar.bind(player.data)
 		gold_counter.bind(player.data)
+		soul_counter.bind(player.data)
